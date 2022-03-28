c5c329b6b35d44965782ab05c22870b6355f397e
Milan Schroeder <milan.schroder@yahoo.de>
2022-03-28 23:12:28 +0200

    now includes scraping of actual articles & function to convert those into data frame

--- 01_scraping_sources.R
+++ 01_scraping_sources.R
@@ -6,11 +6,9 @@ p_load(magrittr)
 p_load(roperators)
 # I use the Internet Archive (IA) to scrape all articles released by RT Deutsch over the year 2021 ####
 
-## get archive entries:
+## get archive entries: (by calender) ####
 
 aktuell <- read_html("https://web.archive.org/web/20210601000000*/de.rt.com/aktuell")
-
-html_elements(aktuell, )
 
 dates <- paste0(2021, rep(sprintf("%02d", 1:12), 31), sprintf("%02d", 1:31)) %>% sort()
 # delete non-existing dates:
@@ -24,13 +22,14 @@ IA_URLS_home <- paste0("https://web.archive.org/web/", dates, "/de.rt.com")
 
 # same across aktuell/home (there also includes "Picture of the Day" and "Feature Video")
 
-
 links_RT <- map_df(.x = IA_URLS_home, .f =  ~ read_html(.) %>% html_elements(".Link-isFullCard"))
 
-# observation: URLS of RT Articles from 2021 have an 6-digit index number starting with 11 (and some with 12) 
-## strategy: scrape all of both, then filter by date 
+
+ 
 
 ### by trying index number (if intended directly from RT): ####
+# observation: URLS of RT Articles from 2021 have an 6-digit index number starting with 11 (and some with 12)
+## strategy: scrape all of both, then filter by date
 # issue: Access blocked frequently + many unnecessary requests
 index <- sprintf("%04d", 0000:9999)
 RT_URLS <- paste0("https://de.rt.com/*/1", rep(c(1,2), each = 10000) , index ,"-*")
@@ -47,7 +46,7 @@ text = session %>% html_elements(".ViewText-root") %>% html_text2(),
 resort = session %>% html_elements(".Breadcrumbs-arrow+ .Breadcrumbs-item span") %>% html_text2(),
 URL = RT_URLS[1]
 )
-### by scraping IA search result list ####
+### WIN: by scraping IA search result list ####
 
 p_load(RSelenium)
 
@@ -58,36 +57,93 @@ rD <- rsDriver(browser = "chrome",
 remDr <- rD[["client"]]
 base_url <- "https://web.archive.org/web/*/de.rt.com*"
 remDr$navigate(base_url)
-src <- remDr$getPageSource()[[1]]
-html_pages[1] <- src
 
-s = 1
+s = 0.5
 total_pages <- 200
-html_pages <- vector("list", total_pages)
+html_tables <- vector("list", total_pages)
+
+src <- remDr$getPageSource()[[1]]
+html_tables[1] <- src
+
+
 
 for (i in 2:total_pages) {
     webElem <- remDr$findElement(using = "css selector", "#resultsUrl_next a")
     webElem$clickElement()
     Sys.sleep(s)
-    html_pages[i] <- remDr$getPageSource()[[1]]
+    html_tables[i] <- remDr$getPageSource()[[1]]
 }
 remDr$close()
 
 ## get contents: ####
 library(rvest)
-RT_links <- map(html_pages, ~ read_html(.) %>% html_table() %>% as.data.frame())
-(IA_links <- map(html_pages, ~ read_html(.) %>% html_elements("#resultsUrl a")) %>% purrr::flatten() 
-  )
-bind_cols(RT_links, IA_links)
+RT_links <- map(html_tables, ~ read_html(.) %>% html_table() %>% as.data.frame())
+IA_links <- map(html_tables, ~ read_html(., encoding = "UTF-8") %>% html_elements("#resultsUrl a")) %>% purrr::flatten()
+
+### ENCODING ERROR !!!!! (concerning few relevant articles but still...)
+
+IA_URLS <- array()
+for (i in 1:length(IA_links)) {
+  IA_URLS[i] <- xml_attrs(IA_links[[i]])
+}
+
 RT_links %<>% bind_rows(., .id = "column_label") %>% select(URL, From, To) %>%
+ mutate(year = str_sub(From, -4, -1) %>% as.numeric(),
+        month = str_sub(From, 1, 3) %>% match(., month.abb),
+        day = str_sub(From, 5, -7) %>% as.numeric() %>% sprintf(fmt = "%02d", .),
+        archived = paste0(year, "/", month, "/", day) %>% as.POSIXlt(., format = "%Y/%m/%d")) %>% 
+  bind_cols(., IA_URLS) %>% 
+    mutate(., IA_URL = paste0("https://web.archive.org", ...8)) %>% 
   # filter for 2021 (and 2022) entries 
-  filter(str_detect(From, "2021") | 
-          str_detect(From, "2022"))
-RT_links %<>% mutate(year = str_sub(From, -4, -1) %>% as.numeric(),
-                      month = str_sub(From, 1, 3) %>% match(., month.abb),
-                      day = str_sub(From, 5, -7) %>% as.numeric() %>% sprintf(fmt = "%02d", .),
-                      archived = paste0(year, "/", month, "/", day) %>% as.POSIXlt(., format = "%Y/%m/%d")) %>% 
-    arrange(archived)
+  filter(between(year, 2021, 2022)) %>% 
+  arrange(archived)
+
+### get first entries of each article: ####
+
+read_html(RT_links$IA_URL[1]) %>% html_elements(".captures-range-info a:nth-child(1)")
+
+
+system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE)
+rD <- rsDriver(browser = "chrome",
+               chromever = "99.0.4844.51")
+remDr <- rD[["client"]]
+
+s = 5
+total_pages <- nrow(RT_links)
+html_pages <- vector("list", total_pages)
+
+for (i in 15:total_pages) {
+  remDr$navigate(RT_links$IA_URL[i])
+  Sys.sleep(s)
+  try(webElem2 <- remDr$findElement(using = "css selector", ".captures-range-info a:nth-child(1)"), 
+      silent = T, outFile = cat("page", i, "failed to navigate"))
+  webElem2$clickElement()
+  html_pages[i] <- remDr$getPageSource()[[1]]
+}
+remDr$close()
+
+# convert to data:
+extract_variables_rt <- function(doc = doc){
+
+# ideally (but hopefully unnecessary): include try/catch
+  df <- data.frame() %>% bind_cols(
+              doc %>%  html_elements(".ArticleView-timestamp .Timestamp-default") %>% html_text2() %>% 
+                tibble() %>% rename(., "date" = "."), 
+              doc %>% html_elements(".HeadLine-type_2") %>% html_text2() %>% 
+                tibble() %>% rename(., "heading" = "."),
+              doc %>% html_elements(".Text-type_1") %>% html_text2() %>% 
+                tibble() %>% rename(., "lead" = "."),
+              doc %>% html_elements(".Breadcrumbs-arrow+ .Breadcrumbs-item span") %>% html_text2() %>% 
+                tibble() %>% rename(., "resort" = "."),
+              doc %>% html_elements(".ViewText-root")%>% html_text2() %>% 
+                tibble() %>% rename(., "text" = "."),
+              id = NA,
+              wordcount = NA
+    ) %>% 
+    select(id, heading, source, resort, wordcount, page, date, text, lead)
+}
+
+df_rt <- map_dfr(html_pages, extract_variables_rt)
 
 # For Comparison, I use the Factiva Database to scrape all articles released online by major national daily newspapers (TAZ, FR, SZ, Welt) ####
 
