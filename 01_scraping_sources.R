1c386c22b46d31d3d801341e6da65670f85e035a
Milan Schroeder <milan.schroder@yahoo.de>
2022-03-28 21:06:21 +0200

    scraped RT URLs from IA

--- 01_scraping_sources.R
+++ 01_scraping_sources.R
@@ -3,20 +3,96 @@
 library(pacman)
 p_load(tidyverse)
 p_load(magrittr)
-
+p_load(roperators)
 # I use the Internet Archive (IA) to scrape all articles released by RT Deutsch over the year 2021 ####
 
 ## get archive entries:
 
+aktuell <- read_html("https://web.archive.org/web/20210601000000*/de.rt.com/aktuell")
+
+html_elements(aktuell, )
+
+dates <- paste0(2021, rep(sprintf("%02d", 1:12), 31), sprintf("%02d", 1:31)) %>% sort()
+# delete non-existing dates:
+noDates <- c(paste0(202102, 29:31), paste0(2021, sprintf("%02d", c(04, 06, 09, 11)), 31))
+dates <- dates[dates %ni% noDates]
+
+IA_URLS_aktuell <- paste0("https://web.archive.org/web/", dates, "/de.rt.com/aktuell")
+IA_URLS_home <- paste0("https://web.archive.org/web/", dates, "/de.rt.com")
+
 ## get article URLs:
 
-## get contents:
+# same across aktuell/home (there also includes "Picture of the Day" and "Feature Video")
 
 
+links_RT <- map_df(.x = IA_URLS_home, .f =  ~ read_html(.) %>% html_elements(".Link-isFullCard"))
+
+# observation: URLS of RT Articles from 2021 have an 6-digit index number starting with 11 (and some with 12) 
+## strategy: scrape all of both, then filter by date 
+
+### by trying index number (if intended directly from RT): ####
+# issue: Access blocked frequently + many unnecessary requests
+index <- sprintf("%04d", 0000:9999)
+RT_URLS <- paste0("https://de.rt.com/*/1", rep(c(1,2), each = 10000) , index ,"-*")
+
+library(httr)
+RT_site <- RT_URLS[1] %>% 
+  html_session(add_headers(`User-Agent`="Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1")) %>% 
+read_html()
+
+test <- tibble(
+header = session %>% html_elements(".HeadLine-type_2") %>% html_text2(),
+date = session %>% html_elements(".ArticleView-timestamp .Timestamp-default") %>% html_text2(),
+text = session %>% html_elements(".ViewText-root") %>% html_text2(),
+resort = session %>% html_elements(".Breadcrumbs-arrow+ .Breadcrumbs-item span") %>% html_text2(),
+URL = RT_URLS[1]
+)
+### by scraping IA search result list ####
+
+p_load(RSelenium)
+
+system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE)
+
+rD <- rsDriver(browser = "chrome",
+               chromever = "99.0.4844.51")
+remDr <- rD[["client"]]
+base_url <- "https://web.archive.org/web/*/de.rt.com*"
+remDr$navigate(base_url)
+src <- remDr$getPageSource()[[1]]
+html_pages[1] <- src
+
+s = 1
+total_pages <- 200
+html_pages <- vector("list", total_pages)
+
+for (i in 2:total_pages) {
+    webElem <- remDr$findElement(using = "css selector", "#resultsUrl_next a")
+    webElem$clickElement()
+    Sys.sleep(s)
+    html_pages[i] <- remDr$getPageSource()[[1]]
+}
+remDr$close()
+
+## get contents: ####
+library(rvest)
+RT_links <- map(html_pages, ~ read_html(.) %>% html_table() %>% as.data.frame())
+(IA_links <- map(html_pages, ~ read_html(.) %>% html_elements("#resultsUrl a")) %>% purrr::flatten() 
+  )
+bind_cols(RT_links, IA_links)
+RT_links %<>% bind_rows(., .id = "column_label") %>% select(URL, From, To) %>%
+  # filter for 2021 (and 2022) entries 
+  filter(str_detect(From, "2021") | 
+          str_detect(From, "2022"))
+RT_links %<>% mutate(year = str_sub(From, -4, -1) %>% as.numeric(),
+                      month = str_sub(From, 1, 3) %>% match(., month.abb),
+                      day = str_sub(From, 5, -7) %>% as.numeric() %>% sprintf(fmt = "%02d", .),
+                      archived = paste0(year, "/", month, "/", day) %>% as.POSIXlt(., format = "%Y/%m/%d")) %>% 
+    arrange(archived)
 
 # For Comparison, I use the Factiva Database to scrape all articles released online by major national daily newspapers (TAZ, FR, SZ, Welt) ####
 
 library(tm)
+library(xml2)
 library(tm.plugin.factiva)
 
 ## retrieve data using:
@@ -89,7 +165,7 @@ library(rvest)
 
 # loop over docs/create list of docs
   # ToDo!
-doc <- read_html("Articles/FAZ_B1.html", encoding = "UTF-8") 
+doc <- read_html("Articles/F.A.Z.-Bibliotheksportal.html", encoding = "UTF-8") 
 
 # function to get variables:
 extract_variables_faz <- function(doc = doc){
